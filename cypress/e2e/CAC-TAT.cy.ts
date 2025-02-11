5341a03eb8fe805cc0aa30fbd7e7498e3147d939
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2025-02-11 15:30:05 -0300

    limpar deu bom

--- cypress/e2e/CAC-TAT.cy.ts
+++ cypress/e2e/CAC-TAT.cy.ts
@@ -1,7 +1,39 @@
+import { delay } from "cypress/types/bluebird";
+
 describe("Central de Atendimento ao Cliente TAT", () => {
-  it("verifica o título da aplicação", () => {
+  beforeEach(() => {
     cy.visit("./src/index.html");
+  });
+  it("verifica o título da aplicação", () => {
     cy.title().should("eq", "Central de Atendimento ao Cliente TAT");
     cy.get("#title").should("contain", "CAC TAT");
+  });
+  it("exibe mensagem de erro ao submeter o formulário com um email com formação inválida", () => {
+    cy.get("#firstName").should("exist").should("be.visible").type("Manel");
+    cy.get("#lastName")
+      .should("exist")
+      .should("be.visible")
+      .type("Moura Silva");
+    cy.get("#email")
+      .should("exist")
+      .should("be.visible")
+      .type("toma esse erro nos peito");
+    cy.get("#phone").should("exist").should("be.visible").type("teste");
+    cy.get("#phone-checkbox").click();
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+  it("limpar os campos ", () => {
+    cy.get("#firstName").type("Manel");
+    cy.get("#lastName").type("Moura Silva");
+    cy.get("#email").type("toma@gmail.com");
+    cy.get("#phone").type("123165");
+    cy.get("#open-text-area").type("exemplo2");
+
+    cy.get("#firstName").clear().should("have.value", "");
+    cy.get("#lastName").clear().should("have.value", "");
+    cy.get("#phone").clear().should("have.value", "");
+    cy.get("#open-text-area").clear().should("have.value", "");
   });
 });
