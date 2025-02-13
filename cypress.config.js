628e32b2a6b3764342ae6aed3ee73fd825651fa7
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2025-02-13 15:53:05 -0300

    aprendi a fazer os videos do teste ririri

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,9 @@ export default defineConfig({
   viewportWidth: 1280,
   e2e: {
     setupNodeEvents(on, config) {},
+
     /*  baseUrl: 'src/index.html'  */
     /*  supportFile: "cypress/support/e2e.ts", */
   },
+  video: true,
 });
--- cypress/e2e/CAC-TAT.cy.ts
+++ cypress/e2e/CAC-TAT.cy.ts
@@ -20,7 +20,7 @@ describe("Central de Atendimento ao Cliente TAT", () => {
       .should("be.visible")
       .type("toma esse erro nos peito");
     cy.get("#phone").should("exist").should("be.visible").type("teste");
-    cy.get("#phone-checkbox").click();
+    cy.get("#phone-checkbox").check();
 
     cy.contains("button", "Enviar").click();
     cy.get(".error").should("be.visible");
@@ -63,5 +63,53 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get('input[type="radio"]').each((typeOfService) => {
       cy.wrap(typeOfService).check().should("be.checked");
     });
+  });
+  it("marca ambos checkboxes, depois desmarca o último", () => {
+    cy.get('input[type="checkbox"]').each((typeOfService) => {
+      cy.wrap(typeOfService).check().should("be.checked");
+    });
+    cy.get('input[type="checkbox"]').last().uncheck().should("not.be.checked");
+    cy.wait(2000);
+    cy.get('input[type="checkbox"]').each((typeOfService) => {
+      cy.wrap(typeOfService).uncheck().should("not.be.checked");
+    });
+  });
+  it("seleciona um arquivo da pasta fixtures", () => {
+    cy.get("#file-upload")
+      .selectFile("cypress/fixtures/example.json")
+      .should(($input) => {
+        const input = $input[0] as HTMLInputElement;
+        expect(input.files?.[0].name).to.equal("example.json");
+      });
+  });
+  it("seleciona um arquivo simulando um drag-and-drop", () => {
+    cy.get("#file-upload")
+      .selectFile("cypress/fixtures/example.json", { action: "drag-drop" })
+      .should("have.prop", "files")
+      .then(($input) => {
+        const files = $input[0] as HTMLInputElement;
+        expect(files.name).to.equal("example.json");
+      });
+  });
+  it("seleciona um arquivo utilizando uma fixture para a qual foi dada um alias", () => {
+    cy.fixture("example.json").as("simpleFile");
+    cy.get("#file-upload")
+      .selectFile("@simpleFile")
+      .should(($input) => {
+        const input = $input[0] as HTMLInputElement;
+        expect(input.files?.[0].name).to.equal("example.json");
+      });
+  });
+  it("verifica que a política de privacidade abre em outra aba sem a necessidade de um clique", () => {
+    cy.contains("a", "Política de Privacidade")
+      .should("have.attr", "href", "privacy.html")
+      .and("have.attr", "target", "_blank");
+  });
+  it("acessa a página da política de privacidade removendo o target e então clicando no link", () => {
+    cy.contains("a", "Política de Privacidade")
+      .invoke("removeAttr", "target")
+      .click();
+    /*    .should("not.have.attr", "target"); */
+    cy.contains("h1", "CAC TAT - Política de Privacidade").should("be.visible");
   });
 });
