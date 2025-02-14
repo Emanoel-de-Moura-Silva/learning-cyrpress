cbb95693f20d6aed5f22a3fca6ffdc8ac1284ecf
Emanoel-de-Moura-Silva <emanuelzx43@gmail.com>
2025-02-14 09:51:38 -0300

    teste 2

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {
-    setupNodeEvents(on, config) {},
-
-    /*  baseUrl: 'src/index.html'  */
-    /*  supportFile: "cypress/support/e2e.ts", */
+    setupNodeEvents() {
+      // Adicione seus eventos aqui
+    },
+    // baseUrl: 'src/index.html', // descomente se necessário
+    // supportFile: "cypress/support/e2e.ts", // descomente se necessário
   },
   video: true,
 });
